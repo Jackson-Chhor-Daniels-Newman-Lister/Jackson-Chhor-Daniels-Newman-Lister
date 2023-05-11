88c6fae60d9375ef44794750a307c8a374577871
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-05-11 11:19:34 -0500

    updated

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -21,7 +21,7 @@ public class ViewProfileServlet extends HttpServlet {
         System.out.println(userAd);
         request.setAttribute("ads", userAd);
 
-
+        //change
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
